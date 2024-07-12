9984fe9cfeff3e501070210ee3042f3621cd893e
karthick <kart87632@gmail.com>
2024-07-12 15:14:43 -0700

    Create a index.js file in Pages folder.

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import FindJobs from "./FindJobs.jsx";
+import AuthPage from "./Auth.jsx";
+import Companies from "./Companies.jsx";
+import UserProfile from "./CompanyProfile.jsx";
+import CompanyProfile from "./CompanyProfile.jsx";
+import UploadJob from "./UploadJob.jsx";
+import About from "./About.jsx";
+import JobDetail from "./JobDetail.jsx";
+
+export {
+    FindJobs,
+    AuthPage,
+    Companies,
+    UserProfile,
+    CompanyProfile,
+    UploadJob,
+    About,
+    JobDetail,
+};
