7e123d8a8f72b1955454d2aa72899d36bcfc2117
Mariaravinth <gm771855@gmail.com>
2024-07-22 12:24:02 +0530

    Created a  UploadJob.jsx in Pages.

--- src/pages/UploadJob.jsx
+++ src/pages/UploadJob.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const UploadJob = () => {
-    return (
-        <div>UploadJob</div>
-    )
+  return <div>UploadJob</div>;
 };
 
 export default UploadJob;
